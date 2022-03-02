102f7226b7fa70989900269d92810f965957db62
LingaPrabu.S <lingaprabunct@gmail.com>
2022-03-02 19:10:20 +0530

    comming soon

--- wwp/src/App.js
+++ wwp/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Preloader from "./components/Preloader/Preloader";
+import Timer from "./components/Countdown/Timer";
+import Optin from "./components/Optin/Optin";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1> Hello </h1>
-      </header>
+      <div className="container">
+        <h1>
+          We Are
+          <br />
+          Coming Soon
+        </h1>
+        <Timer />
+        <Optin />
+        <Preloader />
+      </div>
     </div>
   );
 }
