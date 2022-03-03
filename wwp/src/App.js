5fc7769cf036261b9ecfec5e4579881e7d29d371
LingaPrabu.S <lingaprabunct@gmail.com>
2022-03-03 23:38:05 +0530

    login initiated

--- wwp/src/App.js
+++ wwp/src/App.js
@@ -1,17 +1,26 @@
-import React from "react";
-import Header from "./components/Header.js";
-import "./App.css";
-import Login from "./components/Login";
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages';
+import About from './pages/about';
+import Services from './pages/services';
+import Contact from './pages/contact';
+import SignUp from './pages/signup';
 
 function App() {
-    return ( <div className = "App" >
-        <div className = "container" >
-        <h1> Hello Brother </h1> 
-        <Header />
-        <Login />
-        </div> 
-        </div>
-    );
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/services' element={<Services/>} />
+        <Route path='/contact-us' element={<Contact/>} />
+        <Route path='/sign-up' element={<SignUp/>} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- wwp/src/components/login.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-class Login extends React.Component{
-    render(){
-        return <p>LOGin</p>
-    }
-}
-export default Login;
